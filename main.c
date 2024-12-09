29ffc66401388c137f29096b27371dc7b0b92207
dilshan <jayakody2000lk@gmail.com>
2024-12-09 10:51:11 +0530

    Remove spinlock code.

--- main.c
+++ main.c
@@ -7,44 +7,36 @@
 #include "thread.h"
 #include "scheduler.h"
 #include "timer.h"
-#include "lock.h"
 
-spinlock_t lock = SPINLOCK_UNLOCKED;
-
-int fn(void *arg)
-{
-for(;;) {
-    int i;
-    spinlock_lock(&lock);
-    for(i = 0; i < 80; i++)
-      printk("a = %d\n", i);
-    spinlock_unlock(&lock);
-  }
-  return 6;
+int fn(void *arg) {
+    for (;;) {
+        int i;
+        for (i = 0; i < 80; i++) {
+            printk("a = %d\n", i);
+        }
+    }
+    return 6;
 }
 
-int main(void *mboot_ptr)
-{
+int main(void *mboot_ptr) {
     init_descriptor_tables();
-	init_paging();
+    init_paging();
+    init_timer(20);
 
-    init_timer (20);
+    asm volatile ("sti");
+    init_scheduler(init_threading());
 
-      asm volatile ("sti");
-  init_scheduler (init_threading ());
+    uint32_t *stack = kmalloc(0x400) + 0x3F0;
+    thread_t *t = create_thread(&fn, (void *)0x567, stack);
 
-  uint32_t *stack = kmalloc (0x400) + 0x3F0;
+    for (;;) {
+        int i;
+        for (i = 0; i < 80; i++) {
+            printk("b = %d\n", i);
+        }
+    }
 
-  thread_t *t = create_thread(&fn, (void*)0x567, stack);
-  for(;;) {
-    int i;
-    spinlock_lock(&lock);
-    for(i = 0; i < 80; i++)
-      printk("b = %d\n", i);
-    spinlock_unlock(&lock);
-  }
-
-    for(;;);
+    for (;;);
 
     return 0xDEADBABA;
 }
